2052eea684848e628d5bc238678dd434e64e6d86
fisheeesh <swanphyo444@gmail.com>
2026-05-30 16:26:03 +0700

    chore: logging with loguru

---
+++ backend/app/core/logging.py
@@ -0,0 +1,38 @@
+import os
+from loguru import logger
+from app.core.config import settings
+
+logger.remove()
+
+LOG_DIR = os.path.join(os.path.dirname(os.path.dirname(__file__)), "logs")
+
+LOG_FORMAT = (
+    "{time:YYY-MM_DD HH:mm:ss.SSS} | {level: <8} | {name}:{function}:{line} - {message}"
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "debug.log"),
+    format=LOG_FORMAT,
+    level="DEBUG" if settings.ENVIRONMENT == "local" else "INFO",
+    # ? lambda is a way of defining an anonymous function,
+    # ? in this case we are filtering the logs to only include those with a level of WARNING or higher
+    filter=lambda record: record["level"].no <= logger.level("WARNING").no,
+    rotation="10MB",
+    retention="30 days",
+    compression="zip",
+)
+
+logger.add(
+    sink=os.path.join(LOG_DIR, "error.log"),
+    format=LOG_FORMAT,
+    level="ERROR",
+    rotation="10MB",
+    retention="30 days",
+    compression="zip",
+    backtrace=True,
+    diagnose=True,
+)
+
+
+def get_logger():
+    return logger
