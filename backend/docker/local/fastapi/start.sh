117d12bb63895744c213ad664b84e4696df8f897
fisheeesh <swanphyo444@gmail.com>
2026-06-04 17:16:59 +0700

    chore: minors

--- backend/docker/local/fastapi/start.sh
+++ backend/docker/local/fastapi/start.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# This makes the script to exit immediately if any command exits with a non-zero status,
-# meaning that the script encouters an error
-# If we dun set this, the script will continue executing enve after an error occurs
+# ? This makes the script to exit immediately if any command exits with a non-zero status,
+# ? meaning that the script encouters an error
+# ? If we dun set this, the script will continue executing enve after an error occurs
 set -o errexit
 
-# This unset variables as errors and exit the script immediately
-# This is going to help us to catch any bugs related to undefined variables
+# ? This unset variables as errors and exit the script immediately
+# ? This is going to help us to catch any bugs related to undefined variables
 set -o nounset
 
-# This ensures that the script exists with a non-zero status if any command in a pipeline fails
-# By default, our pipeline exit status is that the last command in the pipeline
+# ? This ensures that the script exists with a non-zero status if any command in a pipeline fails
+# ? By default, our pipeline exit status is that the last command in the pipeline
 set -o pipefail
 
-# 0.0.0.0 tells the server to listen on all available network interfaces
+# ? 0.0.0.0 tells the server to listen on all available network interfaces
 exec uvicorn backend.app.main:app --host 0.0.0.0 --port 8000 --reload
