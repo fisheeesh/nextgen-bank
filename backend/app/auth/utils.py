df66a6a21ebf017e14679355f0e473a407b810b9
fisheeesh <swanphyo444@gmail.com>
2026-06-08 11:04:58 +0700

    chore: otp and password utisl config

---
+++ backend/app/auth/utils.py
@@ -0,0 +1,42 @@
+import random
+import string
+
+from argon2 import PasswordHasher
+from argon2.exceptions import VerifyMismatchError
+
+from ..core.config import settings
+
+_ph = PasswordHasher()
+
+
+def generate_otp(length: int = 6) -> str:
+    otp = "".join(random.choices(string.digits, k=length))
+
+    return otp
+
+
+def generate_password_hash(password: str) -> str:
+    return _ph.hash(password)
+
+
+def verify_password(password: str, hashed_password: str) -> bool:
+    try:
+        return _ph.verify(hashed_password, password)
+    except VerifyMismatchError:
+        return False
+
+
+def generate_username() -> str:
+    bank_name = settings.SITE_NAME
+    words = bank_name.split()
+    # ? NextGen Banking -> NB
+    prefix = "".join([word[0] for word in words]).upper()
+    # ? 12 means username support to be 12 characters long
+    # ? -1 means we are gonna add - between prefix and randmon string
+    remaining_length = 12 - len(*prefix) - 1
+    random_string = "".join(
+        random.choices(string.ascii_uppercase + string.digits, k=remaining_length)
+    )
+    username = f"{prefix}-{random_string}"
+
+    return username
