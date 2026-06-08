ec423abc0486083a55dca35d0fa6f6587e40d12c
fisheeesh <swanphyo444@gmail.com>
2026-06-08 16:59:05 +0700

    feat: create login otp service

---
+++ backend/app/core/services/login_otp.py
@@ -0,0 +1,19 @@
+from ..config import settings
+from ..emails.base import EmailTemplate
+
+
+class LoginOTPEmail(EmailTemplate):
+    template_name = "login_otp.html"
+    template_name_plain = "login_otp.txt"
+    subject = "Your Login OTP"
+
+
+async def send_login_otp_email(email: str, otp: str) -> None:
+    context = {
+        "otp": otp,
+        "expiry_time": settings.OTP_EXPIRATION_MINUTES,
+        "site_name": settings.SITE_NAME,
+        "support_email": settings.SUPPORT_EMAIL,
+    }
+
+    await LoginOTPEmail.send_email(email_to=email, context=context)
