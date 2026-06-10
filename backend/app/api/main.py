b34b34897137b844e3910de7612c5c514837a564
fisheeesh <swanphyo444@gmail.com>
2026-06-10 14:31:10 +0700

    chore: group all auth related routes

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -6,8 +6,8 @@ from .routes.auth import activate, login, password_reset, refresh, register
 api_router = APIRouter()
 
 api_router.include_router(home.router)
-api_router.include_router(register.router)
-api_router.include_router(activate.router)
-api_router.include_router(login.router)
-api_router.include_router(password_reset.router)
-api_router.include_router(refresh.router)
+api_router.include_router(register.router, tags=["Auth"])
+api_router.include_router(activate.router, tags=["Auth"])
+api_router.include_router(login.router, tags=["Auth"])
+api_router.include_router(password_reset.router, tags=["Auth"])
+api_router.include_router(refresh.router, tags=["Auth"])
