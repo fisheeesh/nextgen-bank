e8bd54f260616f9517affc14571e31df3628b8bd
fisheeesh <swanphyo444@gmail.com>
2026-06-10 17:51:17 +0700

    chore: creatre profile related schema

---
+++ backend/app/user_profile/schema.py
@@ -0,0 +1,69 @@
+from datetime import date
+from enum import Enum
+
+from pydantic_extra_types.country import CountryShortName
+from pydantic_extra_types.phone_numbers import PhoneNumber
+from sqlmodel import Field, SQLModel
+
+# from pydantic import field_validator
+# from .utils import validate_id_dates
+
+
+class SalutaionSchema(str, Enum):
+    Mr = "Mr"
+    Mrs = "Mrs"
+    Miss = "Miss"
+
+
+class GenderSchema(str, Enum):
+    Male = "Male"
+    Female = "Female"
+    Other = "Other"
+
+
+class MaritalStatusSchema(str, Enum):
+    Married = "Married"
+    Divorced = "Divorced"
+    Single = "Single"
+    Widowed = "Widowed"
+
+
+class IdentificationTypeSchema(str, Enum):
+    Passport = "Passport"
+    Divers_License = "Drivers_License"
+    Natinal_ID = "National_ID"
+
+
+class EmploymentStatusSchema(str, Enum):
+    Employed = "Employed"
+    Unemployed = "Unemployed"
+    Self_Employed = "Self_Employed"
+    Student = "Student"
+    Retired = "Retired"
+
+
+class ProfileBaseSchema(SQLModel):
+    title: SalutaionSchema
+    gender: GenderSchema
+    date_of_birth: date
+    country_of_birth: CountryShortName
+    place_of_birth: str
+    marital_status: MaritalStatusSchema
+    means_of_identification: IdentificationTypeSchema
+    id_issue_date: date
+    id_expiry_date: date
+    passport_number: str
+    nationality: str
+    phone_number: PhoneNumber
+    address: str
+    city: str
+    country: str
+    employement_status: EmploymentStatusSchema
+    employer_name: str
+    employer_address: str
+    employer_country: CountryShortName
+    annual_income: float
+    date_of_employment: date
+    profile_photo_url: str | None = Field(default=None)
+    id_photo_url: str | None = Field(default=None)
+    signature_photo_url: str | None = Field(default=None)
